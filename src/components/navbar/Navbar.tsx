a85e102e272f62fff2f61df2555bed73d62c888d
Ángel Quintero <angelquinteror102@gmail.com>
2025-12-11 00:04:49 -0500

    Colocar ícono propio y clickeable en el Navbar.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,19 +2,7 @@
 
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@heroui/react";
 import { usePathname } from "next/navigation";
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import Image from "next/image";
 
 export default function NavbarComponent() {
   const pathname = usePathname();
@@ -27,9 +15,14 @@ export default function NavbarComponent() {
         wrapper: "w-full max-w-[1400px] mx-auto px-8 md:px-12 lg:px-16"
       }}
     >
-        <NavbarBrand className="gap-3">
-          <AcmeLogo />
-          <p className="font-bold text-inherit">Ángel Quintero</p>
+        <NavbarBrand as={Link} href="/" className="cursor-pointer">
+          <Image
+            src="/aq.png"
+            alt="Logo"
+            width={130}
+            height={130}
+            className="rounded-md"
+          />
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-10" justify="center">
         <NavbarItem isActive={pathname === "/"}>
