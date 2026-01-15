d92285b6bee47eb2d8e00c4a11b0d4db24a631a5
Ángel Quintero <angelquinteror102@gmail.com>
2026-01-15 15:25:44 -0500

    feat: actualizar página principal para integrar nuevos componentes de secciones y mejorar la estructura del contenido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 ﻿import HeroSection from "../components/sections/HeroSection";
 import AboutSection from "../components/sections/AboutSection";
 import ProjectsSection from "../components/sections/ProjectsSection";
+import ServicesSection from "../components/sections/ServicesSection";
 import CoursesSection from "../components/sections/CoursesSection";
 import ContactSection from "../components/sections/ContactSection";
 import Footer from "../components/footer/Footer";
@@ -13,6 +14,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
+        <ServicesSection />
         <CoursesSection />
         <ContactSection />
         <Footer />
